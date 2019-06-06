3d82fddad697dda1517df2f36f72e5d68c45fc11
james089 <linbojun089@gmail.com>
2019-06-06 14:25:15 -0700

    new app icon, more explaination

--- PictureRenamer/MainWindow.xaml.cs
+++ PictureRenamer/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using mUserControl_BSC_dll.UserControls;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,38 @@ namespace PictureRenamer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Btn_rename_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Directory.Exists(TB_path.Text))
+            {
+                MessageBox.Show("Invalid path!");
+                return;
+            }
+            int index = 0;
+            DirectoryInfo d = new DirectoryInfo(TB_path.Text);
+
+            DirectoryInfo[] ds = d.GetDirectories();
+
+            foreach (DirectoryInfo _d in ds)
+            {
+                FileInfo[] files = _d.GetFiles("*.jpg").OrderBy(p => p.CreationTime).ToArray();
+                foreach (FileInfo f in files)
+                {
+                    using (Bitmap b = new Bitmap(f.FullName))
+                    {
+                        b.Save(TB_path.Text + $"\\Img_{index :D5}.jpg");
+                        index++;
+                    }
+                }
+            }
+            MessageBox.Show("Done!");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
